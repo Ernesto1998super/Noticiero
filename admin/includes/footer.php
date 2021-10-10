9486082b560322f30d8dd0fc7f71fca7033454e1
Ernesto <ernestomac63@hotmail.com>
2021-10-10 17:31:56 -0500

    Proyecto terminado

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                      Alpha Inova
+                </footer>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white"><a href="" target="_blank">Alpha Inova </a></p>
+      </div>
+      <!-- /.container -->
+    </footer>
